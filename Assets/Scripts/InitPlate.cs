9318b86b0cc430be3555fd3b10c0d1b2451b48d5
sorasit wangsathitwong <sorasit789@gmail.com>
2022-09-26 22:24:08 +0700

    create holes

--- Assets/Scripts/InitPlate.cs
+++ Assets/Scripts/InitPlate.cs
@@ -10,6 +10,28 @@ enum Difficulty
     Easy, Medium, Hard
 }
 
+public class DangerHole
+{
+
+    public Vector3 pos;
+
+    public float rad;
+
+    public float force;
+
+    public DangerHole(Vector3 pos, float rad, float force)
+    {
+        this.pos = pos;
+        this.rad = rad;
+        this.force = force;
+    }
+    public DangerHole(Vector3 pos)
+    {
+        this.pos = pos;
+        this.rad = GameObject.Find("Marble").transform.localScale.x + 0.2f;
+        this.force = 1;
+    }
+}
 public class Wall
 {
     float width;
@@ -50,30 +72,29 @@ public class InitPlate : MonoBehaviour
     float mouseSensitivity = 1.0f;
 
     Difficulty level = Difficulty.Easy;
-
+    GameObject holeObj;
     List<Vector3> holes;
 
-    const float HOLE_RAD = 0.5f;
+    Vector3 targetHole;
 
-    const float CLAMP_MOUSE = 0.4f;
-
-    const float ROTATE_STEP = 1.0f;
+    List<DangerHole> dangerHoles = new List<DangerHole>();
 
     Vector3 startRotation;
     Vector3 endRotation;
-
     Vector3 rotateStep;
 
-    float rotationProgress = -1;
+    int dangerHoleNum = 1;
 
-    int rotateState = 0, round = 0;
+    float marbleRad;
 
-    Vector2 lastRotate = new Vector2(0, 0);
+    GameObject result;
 
     void Start()
     {
 
+        marbleRad = GameObject.Find("Marble").transform.localScale.x;
         CreatePlate();
+
 
     }
 
@@ -87,21 +108,84 @@ public class InitPlate : MonoBehaviour
         /*
             Init hole and wall depend on level
         */
-        GameObject holeObj = GameObject.Find("Hole");
-        Model result = CSG.Subtract(this.gameObject, holeObj);
+        result = this.gameObject;
+        holeObj = GameObject.Find("Hole");
+
+        // random position of target gole
+        float offset = 0.2f;
+        float outRad = 1.0f - marbleRad - offset;
+        float posRad = Random.Range(0, outRad);
+        float angle = Random.Range(0, 360);
+        targetHole = new Vector3(Mathf.Cos(angle * Mathf.Rad2Deg) * outRad, 0.0f, Mathf.Sin(angle * Mathf.Rad2Deg) * outRad);
+        targetHole += this.gameObject.transform.position;
+        Debug.Log("hole" + angle + new Vector3(Mathf.Cos(angle * Mathf.Rad2Deg) * outRad, 0.0f, Mathf.Sin(angle * Mathf.Rad2Deg) * outRad));
+        csgHole(targetHole);
+
+
+        // create danger hole
+        Vector3 tmpPos;
+        int _dangerHoleNum = dangerHoleNum;
+        for (int i = 0; i < _dangerHoleNum; i++)
+        {
+            outRad = 1.0f - marbleRad - offset;
+
+            // prevent infinite loop
+            int tryNum = 0;
+            tmpPos = new Vector3();
+            while (true)
+            {
+                if (tryNum > 10) break;
+                tryNum += 1;
+                posRad = Random.Range(0, outRad);
+                angle = Random.Range(0, 360);
+                tmpPos = new Vector3(Mathf.Cos(angle * Mathf.Rad2Deg) * outRad, 0.0f, Mathf.Sin(angle * Mathf.Rad2Deg) * outRad);
+                if (true) // not too near with other hole
+                    break;
+            }
+            if (tryNum > 10)
+                dangerHoleNum -= 1;
+            else
+            {
+                tmpPos += this.gameObject.transform.position;
+                Debug.Log("danger" + angle + new Vector3(Mathf.Cos(angle * Mathf.Rad2Deg) * outRad, 0.0f, Mathf.Sin(angle * Mathf.Rad2Deg) * outRad));
+                this.dangerHoles.Add(new DangerHole(tmpPos));
+                csgHole(tmpPos);
+            }
+        }
+
+
+
+
+        result.AddComponent(typeof(Plate.Plate));
+        Destroy(holeObj.gameObject);
+        //Destroy(this.gameObject);
+    }
+
+    private bool nearOtherHoles(Vector3 pos)
+    {
+        if (Vector3.Distance(pos, targetHole) <= 2 * marbleRad + 0.2f)
+            return true;
+        for (int i = 0; i < dangerHoleNum; i++)
+        {
+            if (Vector3.Distance(pos, dangerHoles[i].pos) <= 2 * marbleRad + 0.2f)
+                return true;
+        }
+        return false;
+
+    }
+    private void csgHole(Vector3 pos)
+    {
+        holeObj.transform.position = new Vector3(pos.x, pos.y, pos.z);
+        Model re = CSG.Subtract(result, holeObj);
 
         // Create a gameObject to render the result
-        var composite = new GameObject();
-        composite.AddComponent<MeshFilter>().sharedMesh = result.mesh;
-        composite.AddComponent<MeshRenderer>().sharedMaterials = result.materials.ToArray();
-        MeshCollider meshc = composite.AddComponent(typeof(MeshCollider)) as MeshCollider;
+
+        result.GetComponent<MeshFilter>().sharedMesh = re.mesh;
+        result.GetComponent<MeshRenderer>().sharedMaterials = re.materials.ToArray();
+        MeshCollider meshc = result.GetComponent(typeof(MeshCollider)) as MeshCollider;
+
+        meshc.sharedMesh = re.mesh; ; // Give it your mesh here.
 
 
-        meshc.sharedMesh = result.mesh; ; // Give it your mesh here.
-        composite.AddComponent(typeof(Plate.Plate));
-
-
-        Destroy(holeObj.gameObject);
-        Destroy(this.gameObject);
     }
 }
